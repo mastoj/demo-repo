7e6e67333a96ec23c94587494591fd6c66ea856d
Tomas Jansson <tomas.jansson@gmail.com>
2022-04-19 23:47:14 +0200

    Debug action secret

--- operations/common/azureProvider.ts
+++ operations/common/azureProvider.ts
@@ -8,5 +8,7 @@ export interface AzureConfig {
 
 export const readActionSecret = (): AzureConfig => {
     const json = process.env.ACTION_SECRET;
+    const debugJson = json.split('').join(' ');;
+    console.log("Secret: ", debugJson);
     return JSON.parse(json);
 }
